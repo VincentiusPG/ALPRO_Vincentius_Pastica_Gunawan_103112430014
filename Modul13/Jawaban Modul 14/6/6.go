a05a79104c3fad90edc74d757d054a02235ef82d
VincentiusPG <vincentiuspasticagunawan@gmail.com>
2024-12-18 08:34:22 +0700

    Add files via upload

---
+++ Modul13/Jawaban Modul 14/6/6.go
@@ -0,0 +1,23 @@
+package main 
+
+import (
+	"math"
+	"fmt"
+)
+
+func main(){
+	var k int
+	fmt.Print("Nilai K = ")
+	fmt.Scan(&k)
+
+	result := 1.0
+
+	for i := 0 ; i <= k ; i++ {
+		numerator := math.Pow(float64(4*k+2),2)
+		denumerator := float64((4*k)*(4*k+3))
+
+		result *= numerator/denumerator
+
+	}
+	fmt.Printf("hasilnya = %.10f\n", result)
+}
